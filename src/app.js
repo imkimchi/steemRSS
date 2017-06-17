d4bed122c2ca35351f37669303918fe103bfa5e9
imkimchi <wbvcos@gmail.com>
2017-06-17 13:20:43 +0900

    add index.html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import Koa from 'koa'
+import serve from 'koa-static'
 import logger from 'koa-logger'
 
 import router from './routes'
 
 const app = new Koa()
 const port = process.env.PORT || 3000
+const dist = path.join(__dirname, '..', 'dist')
+
 
 app
   .use(logger())
   .use(router())
+  .use(serve(dist))
 
 app.listen(port, () => console.log(`[!] Server is Running on ${port}`))
